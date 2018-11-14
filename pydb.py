fe92f50a67c03c0bad669a49c507a3f934da98e7
ndropp92 <ndropp92@aol.com>
2018-11-13 19:46:28 -0500

    Create pydb.py

---
+++ pydb.py
@@ -0,0 +1,17 @@
+import mysql.connector
+
+
+try:
+	cnx = mysql.connector.connect(user='root',password='password',host='127.0.0.1',database='nicksdb')
+	cursor = cnx.cursor()
+	cursor.execute("use nicksdb;")
+
+	cursor.execute('SELECT * FROM employees;')
+	cursor.fetchall()
+	cnx.commit();
+
+except Exception as e:
+	print('error on ' + e)
+
+finally:
+    cnx.close()
